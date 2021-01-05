1509b1272b6ccc145a5f58ba258c24cfece08dde
Cruz <andressmaldonado1@gmail.com>
2021-01-04 20:14:16 -0400

    Primer cambio de el programa usando la linea de comandos.

--- Ejemplo.java
+++ Ejemplo.java
@@ -1,7 +1,12 @@
+import jdk.javadoc.internal.doclets.formats.html.SourceToHTMLConverter;
+
 public class Ejemplo {
 
     public static void main(String args[]){
-System.out.println("hola como estas");
 System.out.println("De nuevo un hola mundo");
+String= casa="esta casa es azul";
+System.out.println("nueva linea");
+System.out.println(casa);
+
     }
 }
